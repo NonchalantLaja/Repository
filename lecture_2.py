9c7a5d54c7d6bcce602a1e7bb2b45dc1ced2a0fc
Olatomiwa Akinlaja <tommyakins520@gmail.com>
2019-08-20 15:53:35 +0200

    Pulling MMM Stock Data

---
+++ lecture_2.py
@@ -0,0 +1,14 @@
+"""LECTURE 2"""
+
+import pandas as pd
+import pandas_datareader.data as pdr
+import datetime as dt
+
+
+start_date = dt.datetime(2010, 1, 1)
+end_date = dt.datetime(2019, 6, 30)
+
+df = pdr.DataReader("MMM", "yahoo", start_date, end_date)
+
+print(start_date, end_date)
+print(df.head())
