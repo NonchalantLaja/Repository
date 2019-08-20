958caa5a230ebb693df5d01a889b73f156a1f203
Olatomiwa Akinlaja <tommyakins520@gmail.com>
2019-08-20 16:25:17 +0200

    Added Changes to Lecture 2 on the new Branch First

--- lecture_2.py
+++ lecture_2.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import pandas_datareader.data as pdr
 import datetime as dt
+import matplotlib.pyplot as plt
 
 
 start_date = dt.datetime(2010, 1, 1)
@@ -12,3 +13,10 @@ df = pdr.DataReader("MMM", "yahoo", start_date, end_date)
 
 print(start_date, end_date)
 print(df.head())
+
+fig = plt.subplot2grid((7, 1), (0, 0), rowspan = 5, colspan = 1)
+fig2 = plt.subplot2grid((7, 1), (5, 0), rowspan = 2, colspan = 1)
+
+fig.plot(df.index, df['Adj Close'])
+fig2.plot(df.index, df['Volume'])
+plt.show()
